97965caaf0c16e69e65e07bf89403c1d5c206de7
mitya47 <8863314@gmail.com>
2023-12-23 02:46:15 +0300

    create new format with image processing

--- src/filter.rs
+++ src/filter.rs
@@ -1,42 +1,92 @@
 use std::path::Path;
-use image::{io::Reader as ImageReader, GenericImageView, Rgb, DynamicImage, Rgba, EncodableLayout};
+use image::{io::Reader as ImageReader, Rgb, ImageBuffer, ImageFormat};
 
-fn get_pixel(img : &DynamicImage, x: i32, y: i32) -> Rgba<u8> {
-  img.get_pixel((x + img.width() as i32) as u32 % img.width(),
-                (y +img.height() as i32) as u32 % img.height())
+struct FilteringImage<'a> {
+  width : i32,
+  height: i32,
+  bytes: &'a [u8]
 }
 
-fn calculate_color(img : &DynamicImage, x: u32, y: u32) -> Rgb<u8> {
-  let mut edited_pixel = Rgb::from([0; 3]);
+impl<'a> FilteringImage<'a> {
+  fn new(bytes: &'a [u8], width: u32, height: u32) -> Self {
+    Self {
+      width: width as i32,
+      height: height as i32,
+      bytes: bytes
+    }
+  }
+}
 
+struct FilteringImageMut<'a> {
+  width : i32,
+  _height: i32,
+  bytes: &'a mut [u8]
+}
+
+impl<'a> FilteringImageMut<'a> {
+  fn new(bytes: &'a mut [u8], width: u32, height: u32) -> Self {
+    Self {
+      width: width as i32,
+      _height: height as i32,
+      bytes: bytes
+    }
+  }
+}
+
+fn get_pixel(img: &FilteringImage, mut x: i32, mut y: i32) -> [u8; 3] {
+  x = (x + img.width) % img.width;
+  y = (y + img.height) % img.height;
+  let first_index = ((y * img.width + x) * 3) as usize;
+  [
+    img.bytes[first_index + 0],
+    img.bytes[first_index + 1],
+    img.bytes[first_index + 2],
+  ]
+}
+
+fn set_pixel(img: &mut FilteringImageMut, x: i32, y: i32, color: [u8; 3]) {
+  let first_index = ((y * img.width + x) * 3) as usize;
+  img.bytes[first_index + 0] = color[0];
+  img.bytes[first_index + 1] = color[1];
+  img.bytes[first_index + 2] = color[2];
+}
+
+fn calculate_pixel<FilterType : super::FilterMatrix>(img: &FilteringImage, x: i32, y: i32, filter: &FilterType) -> [u8; 3] {  
+  let (matrix, coef) = filter.get3x3();
+
+  let mut pixel = [0u8; 3];
   for k in 0..3 {
     let mut sum = 0.0;
     for i in -1..=1 {
       for j in -1..=1 {
-        sum += get_pixel(img, x as i32+ i, y as i32 + j)[k] as f32;
+        sum += get_pixel(img, x + i, y + j)[k] as f32 * matrix[(i + 1) as usize][(j + 1) as usize] as f32;
       }
     }
-    edited_pixel[k] = (sum / 9.0) as u8;
+    pixel[k] = (sum * coef) as u8;
   }
+  pixel
+}
 
-  edited_pixel
+fn apply_filter<FilterType : super::FilterMatrix>(source: &FilteringImage, dest: &mut FilteringImageMut, filter: FilterType) {
+  for x in 0..source.width {
+    for y in 0..source.height {
+      set_pixel(dest, x, y, calculate_pixel(source, x, y, &filter));
+    }
+  }
 }
 
 pub fn filter<FilterType : super::FilterMatrix>(source: &Path, dest: &Path, filter: FilterType) {
-  let img = ImageReader::open(source).unwrap().decode().unwrap();
+  let src_img = ImageReader::open(source).unwrap().decode().unwrap();
+  let mut bytes_vector = vec![0u8; src_img.width() as usize * src_img.height() as usize * 3];
   
-  let width = img.width();
-  let height = img.height();
+  let source_image = FilteringImage::new(src_img.as_bytes(), src_img.width(), src_img.height());
+  let mut dest_image = FilteringImageMut::new(bytes_vector.as_mut_slice(), src_img.width(), src_img.height());
 
-  let mut edited: image::ImageBuffer<Rgb<u8>, Vec<_>> = image::ImageBuffer::new(width, height);
-  for x in 0..width {
-    for y in 0..height {
-      let edited_pixel = edited.get_pixel_mut(x, y);
-      *edited_pixel = calculate_color(&img, x, y);
-    }
-  }
+  apply_filter(&source_image, &mut dest_image, filter);
   
-  image::save_buffer_with_format(dest, &edited, width, height, 
-    image::ColorType::Rgb8, image::ImageFormat::Png).unwrap();
+  let buf: ImageBuffer<Rgb<u8>, _> = 
+    ImageBuffer::from_raw(src_img.width(), src_img.height(), bytes_vector.as_slice()).unwrap();
+  buf.save_with_format(dest, ImageFormat::Png).unwrap();
+
   super::display_images(source, dest);
 }
