f272c9bd9daf633ca4ab0b97c8a900563aa5e15a
mitya47 <8863314@gmail.com>
2023-12-23 13:01:08 +0300

    try compile image

--- src/filter.rs
+++ src/filter.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 use image::{io::Reader as ImageReader, Rgb, ImageBuffer, ImageFormat};
-use super::{FilteringImage, FilteringImageMut};
+use super::FilteringImage;
 
 fn get_pixel(img: &FilteringImage, mut x: i32, mut y: i32) -> [u8; 3] {
   x = (x + img.width) % img.width;
@@ -13,7 +13,7 @@ fn get_pixel(img: &FilteringImage, mut x: i32, mut y: i32) -> [u8; 3] {
   ]
 }
 
-fn set_pixel(img: &mut FilteringImageMut, x: i32, y: i32, color: [u8; 3]) {
+fn set_pixel(img: &mut FilteringImage, x: i32, y: i32, color: [u8; 3]) {
   let first_index = ((y * img.width + x) * 3) as usize;
   img.bytes[first_index + 0] = color[0];
   img.bytes[first_index + 1] = color[1];
@@ -36,25 +36,25 @@ fn calculate_pixel<FilterType : super::FilterMatrix>(img: &FilteringImage, x: i3
   pixel
 }
 
-fn apply_filter<FilterType : super::FilterMatrix>(source: &FilteringImage, dest: &mut FilteringImageMut, filter: FilterType) {
+fn apply_filter<FilterType : super::FilterMatrix>(source: &FilteringImage, dest: &mut FilteringImage, filter: &FilterType) {
   for x in 0..source.width {
     for y in 0..source.height {
-      set_pixel(dest, x, y, calculate_pixel(source, x, y, &filter));
+      set_pixel(dest, x, y, calculate_pixel(source, x, y, filter));
     }
   }
 }
 
-pub fn filter<FilterType : super::FilterMatrix>(source: &Path, dest: &Path, filter: FilterType) {
+pub fn filter<FilterType : super::FilterMatrix>(source: &Path, dest: &Path, filters: Vec<FilterType>) {
   let src_img = ImageReader::open(source).unwrap().decode().unwrap();
   let mut bytes_vector = vec![0u8; src_img.width() as usize * src_img.height() as usize * 3];
   
-  let source_image = FilteringImage::new(src_img.as_bytes(), src_img.width(), src_img.height());
-  let mut dest_image = FilteringImageMut::new(bytes_vector.as_mut_slice(), src_img.width(), src_img.height());
+  let source_image = FilteringImage::copy_from(src_img.as_bytes(), src_img.width(), src_img.height());
+  let mut dest_image = FilteringImage::new(src_img.width(), src_img.height());
 
-  apply_filter(&source_image, &mut dest_image, filter);
+  apply_filter(&source_image, &mut dest_image, &filters[0]);
   
   let buf: ImageBuffer<Rgb<u8>, _> = 
-    ImageBuffer::from_raw(src_img.width(), src_img.height(), bytes_vector.as_slice()).unwrap();
+    ImageBuffer::from_raw(src_img.width(), src_img.height(), dest_image.bytes).unwrap();
   buf.save_with_format(dest, ImageFormat::Png).unwrap();
 
   super::display_images(source, dest);
--- src/filtering_image.rs
+++ src/filtering_image.rs
@@ -1,32 +1,25 @@
-pub struct FilteringImage<'a> {
-  pub height: i32,
+pub struct FilteringImage {
   pub width : i32,
-  pub bytes: &'a [u8],
+  pub height: i32,
+  pub bytes: Vec<u8>,
 }
-  
-impl<'a> FilteringImage<'a> {
-  pub fn new(bytes: &'a [u8], width: u32, height: u32) -> Self {
-    Self {
-      width: width as i32,
-      height: height as i32,
-      bytes: bytes
-    }
+
+impl FilteringImage {
+  pub fn copy_from(bytes:  &[u8], width: u32, height: u32) -> Self {
+    let mut result: Self;
+    result.width = width as i32;
+    result.height = height as i32;
+    result.bytes = Vec::with_capacity((width * height * 3) as usize);
+    result.bytes.copy_from_slice(bytes);
+    result
+  }
+
+  pub fn new(width: u32, height: u32) -> Self {
+    let mut result: Self;
+    result.width = width as i32;
+    result.height = height as i32;
+    result.bytes = vec![0; (width * height * 3) as usize];
+    result
   }
 }
-  
-  pub struct FilteringImageMut<'a> {
-    pub width : i32,
-    pub _height: i32,
-    pub bytes: &'a mut [u8]
-  }
-  
-  impl<'a> FilteringImageMut<'a> {
-    pub fn new(bytes: &'a mut [u8], width: u32, height: u32) -> Self {
-      Self {
-        width: width as i32,
-        _height: height as i32,
-        bytes: bytes
-      }
-    }
-  }
   
