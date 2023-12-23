2b56bfee872c2d1be96d9b8f0bc061ebafaf362a
mitya47 <8863314@gmail.com>
2023-12-23 04:04:25 +0300

    fix negative filter

--- src/filter_types/negative.rs
+++ src/filter_types/negative.rs
@@ -4,9 +4,9 @@ impl super::FilterMatrix for NegativeFilter {
   fn get3x3(&self) -> ([[i8; 3]; 3], f32) {
     (
       [
-        [0, 0, 0],
         [0, -1, 0],
-        [0, 0, 0]
+        [-1, 4, -1],
+        [0, -1, 0]
       ],
       1.0
     )
